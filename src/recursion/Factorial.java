0e44115261629aed3f9d0faa22387bb6457a8d0e
BjornChristensen <bjorn4000@gmail.com>
2026-09-14 14:09:14 +0200

    Bjorn

---
+++ src/recursion/Factorial.java
@@ -0,0 +1,26 @@
+package recursion;
+
+public class Factorial {
+    public static void main(String[] args) {
+
+        System.out.println(f(7));
+    }
+/*
+    static int f(int n) {
+        int result=1;
+        for (int i=1;i<=n;i++) {
+            result=result*i;
+        }
+        return result;
+    }
+ */
+    static int f(int n) {
+        int result;
+        if (n == 1) {
+            result = 1;
+        } else {
+            result = n*f(n-1);
+        }
+        return result;
+    }
+}
